2d68a00362872e2e3536eed69d374bf983302cb5
junaietfaruk <junaietfaruk@throughltd.com>
2026-01-21 18:46:46 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+     <h1>Hellow world</h1>
+     <Name/>
+    </>
+  )
+}
+
+
+function Name(){
+  const age =12;
+  return(
+    <>
+    <h2>i am xunaiet farukk {age} </h2>
+    </>
+  )
+}
+
+
+export default App
